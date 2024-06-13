9f92f194c52f073d59a7ebe18a8f49236edd3734
davidearbola <davidearbola@gmail.com>
2024-06-13 15:06:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log(`Main collegato`);
+
+const { createApp } = Vue;
+
+createApp({
+	data() {
+		return {
+			toDoList: [],
+			valueInput: "",
+			badgeToDo: "ToDo",
+			badgeDone: "Done",
+		};
+	},
+	methods: {
+		addToDo(valore, array) {
+			array.push({ stringa: valore, done: false });
+			this.valueInput = "";
+		},
+		falseToDo(array, indice) {
+			array[indice].done = false;
+		},
+		trueToDo(array, indice) {
+			array[indice].done = true;
+		},
+		removeList() {
+			this.toDoList = [];
+		},
+	},
+	mounted() {
+		axios.get("./api-client/api.php").then((results) => {
+			this.toDoList = results.data;
+		});
+	},
+}).mount("#app");
